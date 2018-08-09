995a3fe3b27f8d786a722b73dbfc589a211fb4b0
jayantbh <bhawal.jayant@gmail.com>
2018-08-10 00:23:18 +0530

    Fix permissions issue with generated index file.

--- bin/build-bin.sh
+++ bin/build-bin.sh
@@ -2,3 +2,4 @@ wp ./bin/index.js -o ./bin/_index.generated.js --target='node';
 cat ./bin/node-shebang.txt > ./bin/index.generated.js;
 cat ./bin/_index.generated.js >> ./bin/index.generated.js;
 rm ./bin/_index.generated.js;
+chmod 755 ./bin/index.generated.js
