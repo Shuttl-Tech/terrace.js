226fbf18315e3fd23d39ef03a648c7aaf30f01a1
jayantbh <bhawal.jayant@gmail.com>
2018-08-10 00:32:46 +0530

    Push with development mode build of bin files.

--- bin/build-bin.sh
+++ bin/build-bin.sh
@@ -1,4 +1,4 @@
-wp ./bin/index.js -o ./bin/_index.generated.js --target='node';
+wp ./bin/index.js -o ./bin/_index.generated.js --target='node' --mode='development';
 cat ./bin/node-shebang.txt > ./bin/index.generated.js;
 cat ./bin/_index.generated.js >> ./bin/index.generated.js;
 rm ./bin/_index.generated.js;
