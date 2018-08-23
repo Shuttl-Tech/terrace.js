6ee945a2e5dc3427ad8e524ce5dff655381ca3ef
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-08-23 15:24:27 +0530

    Fix space before indentation.

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,5 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 // ShallowRenderer: https://reactjs.org/docs/shallow-renderer.html
 it('renders without crashing', () => {
 	const renderer = new ShallowRenderer();
- 	renderer.render(<App />);
+	renderer.render(<App />);
 });
