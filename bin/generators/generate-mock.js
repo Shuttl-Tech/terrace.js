ef687fd74d2c89a938d9c42c9d8d1e6efa57ebfb
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-08-24 15:36:35 +0530

    Add support for creating and removing mocks.

---
+++ bin/generators/generate-mock.js
@@ -0,0 +1,51 @@
+import v from 'voca';
+import pluralize from 'pluralize';
+import { keyInYN } from 'readline-sync';
+const { plural, singular } = pluralize;
+const { snakeCase, lowerCase } = v;
+
+import {
+	checkIfNamePatternIsCorrectOrTerminateProgram,
+	getAllFilesInGeneratedDirectory,
+	getProjectPaths,
+	load,
+	save
+} from './utils/files';
+import { CriticalError } from './utils/error-handlers';
+import { BAD_ARGUMENT_PATTERN, MISSING_ARGUMENT } from './utils/error-codes';
+import { parseTemplateComments } from './utils/template-comments-parser';
+import { existsSync } from 'fs';
+
+const MOCKABLE_ENTITIES = {
+	RESOURCE: 'resource'
+};
+
+export const generateMock = ({ entity, name }) => {
+	let supportedEntities = Object.keys(MOCKABLE_ENTITIES).map(singular).map(lowerCase);
+	const supportedEntitiesMessage = `Supported entities are: ${supportedEntities.map(e => e.underline.bold).join(', ')}`.yellow;
+
+	if (!entity) { CriticalError('🚨 Missing entity type.' + `\n${supportedEntitiesMessage}`, MISSING_ARGUMENT); }
+
+	if (!supportedEntities.includes(entity)) CriticalError(`⚠️  ${entity.bold.underline} is not a valid entity.`.red + `\n${supportedEntitiesMessage}`, BAD_ARGUMENT_PATTERN);
+
+	if (!name) { CriticalError(`🚨 Missing ${entity.toLowerCase()} name. Please specify dasherized ${entity.toLowerCase()} name.`, MISSING_ARGUMENT); }
+	name = checkIfNamePatternIsCorrectOrTerminateProgram({ name });
+
+	let { source, destination } = getProjectPaths();
+
+	let templatePath = `${source}/bin/generators/templates/mocks/${plural(entity)}/generic.js`;
+	let resourcePath = `${destination}/src/mocks/${plural(entity)}`;
+	let fileName = `${name}.js`;
+
+	if (existsSync(`${resourcePath}/${fileName}`)) {
+		let doOverwrite = keyInYN('⚠️  File already exists. Overwrite? [Y/N] ');
+		if (!doOverwrite) process.exit();
+	}
+
+	let mock = 	load(templatePath).process({ resourceName: snakeCase(name).toUpperCase() });
+	mock = parseTemplateComments({ file: mock });
+	save(`${resourcePath}/${fileName}`, mock);
+
+	console.log('✅ Files created:'.bold.cyan.underline);
+	console.log(getAllFilesInGeneratedDirectory(resourcePath));
+};
--- bin/generators/remove-entity.js
+++ bin/generators/remove-entity.js
@@ -1,13 +1,14 @@
 import v from 'voca';
-import fs from 'file-system';
+import { rmdirSync } from 'file-system';
+import { unlinkSync, lstatSync } from 'fs';
 import pluralize from 'pluralize';
 const { plural, singular } = pluralize;
 
-import { getProjectPaths, SUPPORTED_ENTITIES } from './utils/files';
+import { ENTITY, getProjectPaths, SUPPORTED_ENTITIES } from './utils/files';
 import { CriticalError } from './utils/error-handlers';
 import { BAD_ARGUMENT_PATTERN, MISSING_ARGUMENT } from './utils/error-codes';
 
-export const removeEntity = ({ entity, name }) => {
+export const removeEntity = ({ entity, name_or_subtype: nameOrSubtype, name }) => {
 	let supportedEntities = SUPPORTED_ENTITIES.map(singular);
 	const supportedEntitiesMessage = `Supported entities are: ${supportedEntities.map(e => e.underline.bold).join(', ')}`.yellow;
 
@@ -15,14 +16,28 @@ export const removeEntity = ({ entity, name }) => {
 
 	if (!supportedEntities.includes(entity)) CriticalError(`⚠️  ${entity.bold.underline} is not a valid entity.`.red + `\n${supportedEntitiesMessage}`, BAD_ARGUMENT_PATTERN);
 
+	let hasSubtype, subtype, isSingleJSFile;
+	switch (entity) {
+		case singular(ENTITY.MOCKS): hasSubtype = isSingleJSFile = true; break;
+		default: break;
+	}
+
+	if (!hasSubtype) name = nameOrSubtype;
+	else {
+		subtype = nameOrSubtype;
+		if (!subtype) { CriticalError(`🚨 Missing ${entity.toLowerCase()} subtype. Please specify dasherized ${entity.toLowerCase()} subtype.`, MISSING_ARGUMENT); }
+	}
+
 	if (!name) { CriticalError(`🚨 Missing ${entity.toLowerCase()} name. Please specify dasherized ${entity.toLowerCase()} name.`, MISSING_ARGUMENT); }
 
 	let { destination } = getProjectPaths();
 
-	let entityComponentName = v.titleCase(name).replace(/-/g, '');
-	let path = `${destination}/src/${plural(entity)}/${entityComponentName}`;
+	let entityTargetName = hasSubtype ? name : v.titleCase(name).replace(/-/g, '');
+	let path = `${destination}/src/${plural(entity)}`
+		+ `${hasSubtype ? `/${plural(subtype)}` : ''}/${entityTargetName}`
+		+ `${isSingleJSFile ? '.js' : ''}`;
 
-	console.log(`⏳ Removing ${entity} ${entityComponentName.underline}`.green.bold);
-	fs.rmdirSync(path);
+	console.log(`⏳ Removing ${entity} ${entityTargetName.underline}`.green.bold);
+	lstatSync(path).isDirectory() ? rmdirSync(path) : unlinkSync(path) ;
 	console.log(`✅ Entity removed.`.green);
 };
