012d44da941443b692c53b3a84325d976c166fcd
Jayant Bhawal <bhawal.jayant@gmail.com>
2018-12-24 22:30:15 +0530

    Make SubDir files transformer extension agnostic.

--- bin/generators/utils/subdir-files-transformer.js
+++ bin/generators/utils/subdir-files-transformer.js
@@ -2,16 +2,18 @@ import through from 'through2';
 import { parseTemplateComments } from './template-comments-parser';
 
 export const subdirFilesTransformer = (src, { source, withoutI18n }) => {
-	const subdirFileName = src.slice(source.length + 1);
+	const subdirFileName = src.slice(source.length + 1, src.lastIndexOf('.'));
 	let file;
 	return through((chunk, enc, done) => {
 		file = chunk.toString();
+
+		// File names without extensions
 		switch (subdirFileName) {
-			case 'src/index.js':
-			case 'src/setupTests.js':
-			case 'src/views/Home/index.js':
-			case 'src/components/ParametrizedView/index.js':
-			case 'src/views/Home/__tests__/index.test.js':
+			case 'src/index':
+			case 'src/setupTests':
+			case 'src/views/Home/index':
+			case 'src/components/ParametrizedView/index':
+			case 'src/views/Home/__tests__/index.test':
 				file = parseTemplateComments({ tokens: ['with-i18n'], file, invert: withoutI18n });
 				break;
 			default: break;
